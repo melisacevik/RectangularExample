ce7969b9b4ea3935494cd4918af572489ec236e8
melisacevik <celissamevik@gmail.com>
2022-12-29 17:08:10 +0300

    RectangularExample

---
+++ RectangularExample/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        double ukenar = 1.0, kkenar = 1.0, alan = 1.0, cevre = 1.0;
+
+        Console.WriteLine("Uzun kenari giriniz:");
+        ukenar = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Kisa kenari giriniz:");
+        kkenar = Convert.ToDouble(Console.ReadLine());
+
+        alan = ukenar * kkenar;
+        cevre = (ukenar + kkenar) * 2;
+
+        Console.WriteLine("\nCevre= {0} \n\nAlan= {1} ", cevre, alan);
+        Console.ReadKey();
+
+
+    }
+}
